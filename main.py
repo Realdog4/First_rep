5157d7beacda475ec7b1a44107b3a08e6ef20888
Bogdan <sirbony95@gmail.com>
2023-04-10 20:54:07 +0300

    add def parse cookie

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from http.cookies import SimpleCookie
+
+
 def parse(query: str) -> dict:
     return {}
 
@@ -11,7 +14,10 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    cookie = SimpleCookie()
+    cookie.load(query)
+    result = {k: v.value for k, v in cookie.items()}
+    return result
 
 
 if __name__ == '__main__':
@@ -19,3 +25,12 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('key=value;name=Dima;') == {'key': 'value', 'name': 'Dima'}
+    assert parse_cookie('name=Dima;age=28;city=New+York;') == {'name': 'Dima', 'age': '28', 'city': 'New+York'}
+    assert parse_cookie('username=johndoe;sessionid=1234567890;') == {'username': 'johndoe', 'sessionid': '1234567890'}
+    assert parse_cookie('name=Dima;age=28;email=dima@example.com;') == {'name': 'Dima', 'age': '28', 'email': 'dima@example.com'}
+    assert parse_cookie('user_id=1234;first_name=John;last_name=Doe;') == {'user_id': '1234', 'first_name': 'John', 'last_name': 'Doe'}
+    assert parse_cookie('username=admin;password=12345;is_admin=true;') == {'username': 'admin', 'password': '12345','is_admin': 'true'}
+    assert parse_cookie('username=johndoe;sessionid=1234567890;lang=en-US;') == {'username': 'johndoe','sessionid': '1234567890','lang': 'en-US'}
+    assert parse_cookie('foo=bar;baz=qux;') == {'foo': 'bar', 'baz': 'qux'}
+    assert parse_cookie('name=Dima=Smith;') == {'name': 'Dima=Smith'}
